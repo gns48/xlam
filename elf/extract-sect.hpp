4fc9a6a80bf8da6bac7f432fd02b30bbf436a8a7
Gleb Semenov <gleb.semenov@gmail.com>
2020-11-29 12:48:58 +0300

    templates specialization removed

--- elf/extract-sect.hpp
+++ elf/extract-sect.hpp
@@ -11,29 +11,19 @@
 //ofset, size
 typedef std::pair<off_t, size_t> section_t;
 
-template <typename T> class ElfDumper {
-    const char* m_eheader; // ptr to ELF header (either Elf32_Ehdr or Elf64_Ehdr)
-    off_t m_shoff;         // Section table offset
-    size_t m_shentsize;    // Section table entry size
-    int m_shnum;           // Section table entry count
-    const char* m_strings; // Section names offset
+template <typename T, typename S> class ElfDumper {
+    const char* m_eheader;  // ptr to ELF header (either Elf32_Ehdr or Elf64_Ehdr)
+    int m_shnum;            // Section table entry count
+    const S* m_sections;    // sections header list
+    const char* m_strings;  // Section names offset
     
     // section iterator
     // find section with specified name and return offset & size
     // return <0, 0> if section not found
     const section_t findSection(const char* sname) const {
-        if constexpr (std::is_same_v<T, Elf64_Ehdr>) {
-           const Elf64_Shdr* sections = (const Elf64_Shdr*)(m_eheader + m_shoff);
-           for(int i = 0; i < m_shnum; i++)
-               if(strcmp(m_strings + sections[i].sh_name, sname) == 0)
-                   return section_t {sections[i].sh_offset, sections[i].sh_size};
-        }
-        else {
-            const Elf32_Shdr* sections = (const Elf32_Shdr*)(m_eheader + m_shoff);
-            for(int i = 0; i < m_shnum; i++)
-               if(strcmp(m_strings + sections[i].sh_name, sname) == 0)
-                   return section_t {sections[i].sh_offset, sections[i].sh_size};
-        }
+        for(int i = 0; i < m_shnum; i++)
+            if(strcmp(m_strings + m_sections[i].sh_name, sname) == 0)
+                return section_t {m_sections[i].sh_offset, m_sections[i].sh_size};
         return section_t {0, 0};
     }
     
@@ -44,24 +34,12 @@ public:
     explicit ElfDumper(const void* header)
         : m_eheader(reinterpret_cast<const char*>(header))
     {
-        m_shoff =     ((const T*)m_eheader)->e_shoff;
-        m_shentsize = ((const T*)m_eheader)->e_shentsize;
-        m_shnum =     ((const T*)m_eheader)->e_shnum;
-        
-        if constexpr (std::is_same_v<T, Elf64_Ehdr>) {
-            const Elf64_Shdr* sections = (const Elf64_Shdr*)(m_eheader + m_shoff);
-            const Elf64_Shdr* ssect_header = &sections[((const T*)m_eheader)->e_shstrndx];
-            m_strings = m_eheader + ssect_header->sh_offset;
-        }
-        else if constexpr (std::is_same_v<T, Elf32_Ehdr>) {
-            const Elf32_Shdr* sections = (const Elf32_Shdr*)(m_eheader + m_shoff);
-            const Elf32_Shdr* ssect_header = &sections[((const T*)m_eheader)->e_shstrndx];
-            m_strings = m_eheader + ssect_header->sh_offset;
-        }
-        else
-            throw std::logic_error("Unsupported ELF header type");
+        m_shnum    = ((const T*)m_eheader)->e_shnum;
+        m_sections = (const S*)(m_eheader + ((const T*)m_eheader)->e_shoff);
+        const S* ssect_header = &m_sections[((const T*)m_eheader)->e_shstrndx];
+        m_strings = m_eheader + ssect_header->sh_offset;
     }
-    
+
     void dumpSection(const char* sname, const char* fname) const {
         const auto& [off, size] = findSection(sname);
         if(off) { // section found
