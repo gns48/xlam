7ce6be50d81b28fd54fc5614efae15efc9413f24
Gleb Semenov <gleb.semenov@gmail.com>
2021-02-05 20:44:49 +0300

    usage line and simple comman line parameters check added

--- guitar-strings/psql-create.sh
+++ guitar-strings/psql-create.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
 tables="tension_kinds strings hannabach_tension tensions"
+usage="Usage: $0 [<schema name>]"
+
+case $1 in
+    "-h"|"-?"|"--help"|"-help")
+        echo "$usage"
+        exit 0
+        ;;
+    -*)
+        echo "Bad option"
+        echo "$usage"
+        exit 1
+        ;;
+    *)
+        if [ -n "$1" ] ; then
+            schema="$1"
+        fi
+        ;;
+esac
 
 for t in $tables ; do
 	psql -c "DROP TABLE IF EXISTS $t CASCADE"
@@ -12,7 +30,11 @@ for t in $tables ; do
 	psql -c "copy $t FROM STDIN (FORMAT 'csv', DELIMITER ',')" < $t.csv
 done
 
-if [ -n "$1" ] ; then
-	psql -c "DROP SCHEMA IF EXISTS $1 CASCADE; CREATE SCHEMA $1"
-	for t in $tables ; do psql -c "ALTER TABLE $t SET SCHEMA $1" ; done
+if [ -n "$schema" ] ; then
+	psql -c "DROP SCHEMA IF EXISTS $schema CASCADE; CREATE SCHEMA $schema"
+	for t in $tables ; do psql -c "ALTER TABLE $t SET SCHEMA $schema" ; done
 fi
+
+
+
+
